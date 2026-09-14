85f047c0bc139a255c0882a218f467b877985fc5
Jakub Šepeľa <jakubsepik@gmail.com>
2026-09-14 14:58:49 +0200

    change id lenght for QRCL

--- candelabra/candelabra/doctype/qr_code_link/qr_code_link.py
+++ candelabra/candelabra/doctype/qr_code_link/qr_code_link.py
@@ -3,4 +3,10 @@ from frappe.model.document import Document
 
 
 class QRCodeLink(Document):
-    pass
+    def autoname(self):
+        for _ in range(5):
+            name = frappe.generate_hash(length=6)
+            if not frappe.db.exists("QR Code Link", name):
+                self.name = name
+                return
+        frappe.throw("Nepodarilo sa vygenerovať unikátne QR ID")
